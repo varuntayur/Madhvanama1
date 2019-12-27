681aae0e12a833622f5404061c98fcb408e1e46d
Varun Tayur <tayur@synopsys.com>
2019-12-27 15:08:46 +0530

    Initial commit

---
+++ app/src/main/java/com/vtayur/madhvanama/MainActivity.java
@@ -0,0 +1,141 @@
+package com.vtayur.madhvanama;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.vtayur.madhvanama.data.DataProvider;
+import com.vtayur.madhvanama.data.Language;
+import com.vtayur.madhvanama.data.YesNo;
+import com.vtayur.madhvanama.detail.common.CustomAdapter;
+import com.vtayur.madhvanama.detail.settings.SettingsActivity;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "HomeActivity";
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        progressDialog = ProgressDialog.show(this, "", getResources().getString(R.string.loading_please_wait), true);
+
+        DataProvider.init(getAssets());
+        initPreferences();
+        initLayout();
+
+        progressDialog.dismiss();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.help:
+                View messageView = getLayoutInflater().inflate(R.layout.about, null, false);
+
+                TextView textView = (TextView) messageView.findViewById(R.id.about_credits);
+                int defaultColor = textView.getTextColors().getDefaultColor();
+                textView.setTextColor(defaultColor);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setIcon(R.drawable.suktas);
+                builder.setTitle(R.string.app_name);
+                builder.setView(messageView);
+                builder.create();
+                builder.show();
+
+                return true;
+            case R.id.settings:
+
+                Intent intent = new Intent(this, SettingsActivity.class);
+                Log.d(TAG, "Launching Settings Activity");
+                startActivity(intent);
+
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void launchMadhvanama(View v) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.setData(Uri.parse("market://details?id=com.vtayur.madhvanama"));
+        startActivity(intent);
+    }
+
+    private void initLayout() {
+        final List<String> sectionNames = DataProvider.getMenuNames();
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (position > 0)
+                    return 1;
+                else
+                    return 2;
+            }
+        });
+        CustomAdapter customAdapter = new CustomAdapter(this, sectionNames);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.getRecycledViewPool().clear();
+        recyclerView.setAdapter(customAdapter); // set the Adapter to RecyclerView
+
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private void initPreferences() {
+        SharedPreferences settings = getSharedPreferences(DataProvider.PREFS_NAME, 0);
+        String isLocalLangAlreadySaved = settings.getString(DataProvider.SHLOKA_DISP_LANGUAGE, "");
+        String isLearningModeSaved = settings.getString(DataProvider.LEARNING_MODE, "");
+        if (isLocalLangAlreadySaved.isEmpty()) {
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString(DataProvider.SHLOKA_DISP_LANGUAGE, Language.san.toString());
+
+            editor.commit();
+
+            Log.d(TAG, "Setting the default launch language preference to Sanskrit at startup - " + settings.getString(DataProvider.SHLOKA_DISP_LANGUAGE, ""));
+        }
+
+        if (isLearningModeSaved.isEmpty()) {
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString(DataProvider.LEARNING_MODE, YesNo.yes.toString());
+
+            editor.commit();
+
+            Log.d(TAG, "Setting the default learn mode preference to 'Yes' - " + settings.getString(DataProvider.LEARNING_MODE, ""));
+        }
+    }
+}
---
+++ app/src/main/java/com/vtayur/madhvanama/data/model/Shloka.java
@@ -0,0 +1,63 @@
+package com.vtayur.madhvanama.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import org.simpleframework.xml.Root;
+
+import java.io.Serializable;
+
+/**
+ * Created by varuntayur on 4/5/2014.
+ */
+@Root
+public class Shloka implements Serializable {
+
+
+    public static final String EMPTY_STRING = "";
+
+    @SerializedName("num")
+    private String num;
+
+    @SerializedName("text")
+    private String text;
+
+    @SerializedName("explanation")
+    private ShlokaDescription explanation;
+
+    public Shloka() {
+    }
+
+    @Override
+    public String toString() {
+        return "Shloka{" +
+                "num='" + num + '\'' +
+                ", text='" + text + '\'' +
+                ", explanation=" + explanation +
+                '}';
+    }
+
+    public String getText() {
+        if (text != null) {
+
+            StringBuilder sbuilder = new StringBuilder();
+            String[] split = text.split("\n");
+            for (String splitStr : split) {
+                sbuilder.append(splitStr.trim());
+                sbuilder.append("\n");
+            }
+            return sbuilder.toString().trim();
+        }
+        return EMPTY_STRING;
+    }
+
+
+    public String getFormattedExplanation() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Note note : explanation.getNotesList()) {
+            stringBuilder.append(note.getFormattedNote());
+        }
+        return stringBuilder.toString();
+    }
+
+}
